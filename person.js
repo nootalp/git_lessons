cf5a0316c481483d77e89676fcfae20cdaed1848
Gabriel Gomes <gomestatschgabriel@gmail.com>
2022-12-02 11:27:34 -0300

    Git learned.

---
+++ person.js
@@ -0,0 +1,13 @@
+// Modules imports on practice;
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  sayMyName() {
+    return `My name is ${this.name}!`;
+  }
+}
+
+module.exports = {
+  Person,  
+};
